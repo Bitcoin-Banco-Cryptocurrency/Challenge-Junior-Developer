3636f19b3bd412c2bba1e227255d7521bb20a420
Rafael Toshio <rtu.dev@gmail.com>
2018-08-30 06:51:44 -0300

    Desafio

---
+++ BB/node-str/src/routes/index.js
@@ -0,0 +1,13 @@
+'use stric'
+
+const express = require('express');
+const route = express.Router();
+/*
+route.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: 'Node Store API',
+        version: '0.0.3'
+    });
+});
+*/
+module.exports = route;
